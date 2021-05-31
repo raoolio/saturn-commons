759c4c8ba026662931ae5002106862b3ce6c76a8
Raul del Cid <rdelcid@televida.biz>
2021-05-31 17:11:59 -0600

    Minor code improvements: - Added @Component tag for spring auto configuration. - Bumped spring-context version to 5.3.7

--- saturn-commons-spring/src/main/java/com/saturn/commons/spring/ApplicationContextProvider.java
+++ saturn-commons-spring/src/main/java/com/saturn/commons/spring/ApplicationContextProvider.java
@@ -4,7 +4,9 @@ package com.saturn.commons.spring;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ApplicationContextProvider implements ApplicationContextAware {
 
     /** ApplicationContext instance */
